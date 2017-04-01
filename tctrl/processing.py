daf8550760c93f163ec4ed8f2c8e21b482ae1440
t3kt <tommy.etkin@gmail.com>
2017-04-01 15:01:58 -0700

    added tool for generating param/child group lists see #7

--- tctrl/processing.py
+++ tctrl/processing.py
@@ -1,6 +1,10 @@
 from tctrl.schema import *
 import copy
 
+class ErrorHandler:
+	def OnMissingList(self, param):
+		pass
+
 def WalkChildModules(node, action):
 	if node.children:
 		for child in node.children:
@@ -12,6 +16,8 @@ def ProcessAppSchema(appschema:AppSchema,
 										 striplists=None,
 										 embedmoduletypes=False,
 										 stripmoduletypes=None,
+										 generateparamgroups=False,
+										 generatechildgroups=False,
 										 errorhandler=None):
 	if striplists is None:
 		striplists = embedlists
@@ -19,32 +25,18 @@ def ProcessAppSchema(appschema:AppSchema,
 		stripmoduletypes = embedmoduletypes
 
 	appschema = copy.deepcopy(appschema)
-	optionlistsbykey = {
-		l.key: l for l in appschema.optionlists or []
-		} if embedlists else {}
 
-	moduletypesbykey = {
-		t.key: t for t in appschema.moduletypes or []
-	} if embedmoduletypes else None
+	if embedmoduletypes:
+		_EmbedModuleTypes(appschema, errorhandler)
 
-	def _moduleAction(module:ModuleSpec, parent=None, **kwargs):
-		if embedmoduletypes and module.moduletype and module.moduletype in moduletypesbykey:
-			modtype = moduletypesbykey[module.moduletype]
-			if not module.params:
-				module.params = copy.deepcopy(modtype.params)
-			if not module.paramgroups:
-				module.paramgroups = copy.deepcopy(modtype.paramgroups)
+	if embedlists:
+		_EmbedSchemaLists(appschema, errorhandler)
 
-		if embedlists and optionlistsbykey:
-			for param in module.params:
-				if param.optionlist and not param.options:
-					if param.optionlist not in optionlistsbykey:
-						if errorhandler:
-							errorhandler.OnMissingList(param.optionlist, param=param)
-						continue
-					param.options = copy.deepcopy(optionlistsbykey[param.optionlist].options)
+	if generateparamgroups:
+		_GenerateParamGroups(appschema)
 
-	WalkChildModules(appschema, _moduleAction)
+	if generatechildgroups:
+		_GenerateChildGroups(appschema)
 
 	if striplists:
 		appschema.optionlists = []
@@ -53,7 +45,59 @@ def ProcessAppSchema(appschema:AppSchema,
 
 	return appschema
 
-class ErrorHandler:
-	def OnMissingList(self, param):
-		pass
 
+def _EmbedModuleTypes(appschema:AppSchema,
+											errorhandler=None):
+	if not appschema.moduletypes:
+		return
+	moduletypesbykey = {t.key: t for t in appschema.moduletypes}
+	def _moduleAction(module:ModuleSpec, **kwargs):
+		if module.moduletype and module.moduletype in moduletypesbykey:
+			modtype = moduletypesbykey[module.moduletype]
+			if not module.params:
+				module.params = copy.deepcopy(modtype.params)
+			if not module.paramgroups:
+				module.paramgroups = copy.deepcopy(modtype.paramgroups)
+	WalkChildModules(appschema, _moduleAction)
+
+
+def _EmbedSchemaLists(appschema:AppSchema,
+											errorhandler:ErrorHandler):
+	if not appschema.optionlists:
+		return
+	optionlistsbykey = {l.key: l for l in appschema.optionlists}
+	def _moduleAction(module:ModuleSpec, **kwargs):
+		for param in module.params:
+			if param.optionlist and not param.options:
+				if param.optionlist not in optionlistsbykey:
+					if errorhandler:
+						errorhandler.OnMissingList(param)
+					continue
+				param.options = copy.deepcopy(optionlistsbykey[param.optionlist].options)
+	WalkChildModules(appschema, _moduleAction)
+
+def _GenerateParamGroups(appschema:AppSchema):
+	def _moduleAction(module:ModuleSpec, **kwargs):
+		if module.params:
+			module.paramgroups = _GenerateGroups(module.params, module.paramgroups)
+	WalkChildModules(appschema, _moduleAction)
+
+def _GenerateChildGroups(appschema:AppSchema):
+	def _moduleAction(module, **kwargs):
+		if module.children:
+			module.childgroups = _GenerateGroups(module.children, module.childgroups)
+	_moduleAction(appschema)
+	WalkChildModules(appschema, _moduleAction)
+
+def _GenerateGroups(nodes, groups):
+	groups = groups or []
+	if not nodes:
+		return groups
+	knowngroups = {g.key for g in groups}
+	for node in nodes:
+		if node.group and node.group not in knowngroups:
+			groups.append(GroupInfo(
+				node.group,
+				label=node.group))
+			knowngroups.add(node.group)
+	return groups
